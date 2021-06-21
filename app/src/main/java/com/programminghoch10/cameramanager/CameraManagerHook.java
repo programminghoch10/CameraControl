58377df22c6caea86a13636eda7f94e70f60cf44
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-06-21 22:27:39 +0200

    made hooks honor preferences

--- app/src/main/java/com/programminghoch10/cameramanager/CameraManagerHook.java
+++ app/src/main/java/com/programminghoch10/cameramanager/CameraManagerHook.java
@@ -1,5 +1,6 @@
 package com.programminghoch10.cameramanager;
 
+import android.content.SharedPreferences;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
@@ -12,49 +13,61 @@ import java.util.Iterator;
 import java.util.List;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class CameraManagerHook {
-	private static final String TAG = "CameraManager";
 	
-	static void hook(XC_LoadPackage.LoadPackageParam lpparam) {
-		XposedBridge.log("Hooking getCameraIdList");
-		XposedHelpers.findAndHookMethod("android.hardware.camera2.CameraManager", lpparam.classLoader, "getCameraIdList", new XC_MethodHook() {
-			@Override
-			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				String[] ids = (String[]) param.getResult();
-				String s = Arrays.toString(ids);
-				//Log.d(TAG, "getCameraIdList: " + s);
-				//XposedBridge.log("getCameraIdList: " + s);
-				CameraManager cameraManager = (CameraManager) param.thisObject;
-				List<String> cameras = new ArrayList<>(Arrays.asList(ids));
-				Iterator<String> iterator = cameras.iterator();
-				while (iterator.hasNext()) {
-					String id = iterator.next();
-					CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(id);
-					if (characteristics.get(CameraCharacteristics.LENS_FACING).equals(CameraCharacteristics.LENS_FACING_FRONT))
-						iterator.remove();
+	static void hook(XC_LoadPackage.LoadPackageParam lpparam, SharedPreferences sharedPreferences) {
+		if (sharedPreferences.getBoolean("blockList", true)) {
+			XposedBridge.log("Hooking getCameraIdList");
+			XposedHelpers.findAndHookMethod("android.hardware.camera2.CameraManager", lpparam.classLoader, "getCameraIdList", new XC_MethodHook() {
+				@Override
+				protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+					String[] ids = (String[]) param.getResult();
+					CameraManager cameraManager = (CameraManager) param.thisObject;
+					List<String> cameras = new ArrayList<>(Arrays.asList(ids));
+					Iterator<String> iterator = cameras.iterator();
+					while (iterator.hasNext()) {
+						String id = iterator.next();
+						CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(id);
+						if (disableCamera(characteristics, sharedPreferences))
+							iterator.remove();
+					}
+					String[] camerasModified = cameras.toArray(new String[0]);
+					param.setResult(camerasModified);
 				}
-				String[] camerasModified = cameras.toArray(new String[0]);
-				param.setResult(camerasModified);
-				//Log.d(TAG, "getCameraIdList: after modification " + Arrays.toString(camerasModified));
-			}
-		});
-		XposedBridge.log("Hooking openCamera");
-		Method openCamera = XposedHelpers.findMethodExact("android.hardware.camera2.CameraManager", lpparam.classLoader, "openCamera",
-				String.class, "android.hardware.camera2.CameraDevice$StateCallback", Handler.class);
-		XposedBridge.hookMethod(openCamera, new XC_MethodHook() {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				String cameraId = (String) param.args[0];
-				//Log.d(TAG, "openCamera: " + cameraId);
-				CameraManager cameraManager = (CameraManager) param.thisObject;
-				CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
-				if (characteristics.get(CameraCharacteristics.LENS_FACING).equals(CameraCharacteristics.LENS_FACING_FRONT))
-					param.setThrowable(new CameraAccessException(CameraAccessException.CAMERA_DISABLED));
-			}
-		});
+			});
+		}
+		if (sharedPreferences.getBoolean("blockAccess", true)) {
+			XposedBridge.log("Hooking openCamera");
+			Method openCamera = XposedHelpers.findMethodExact("android.hardware.camera2.CameraManager", lpparam.classLoader, "openCamera",
+					String.class, "android.hardware.camera2.CameraDevice$StateCallback", Handler.class);
+			XposedBridge.hookMethod(openCamera, new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					String cameraId = (String) param.args[0];
+					CameraManager cameraManager = (CameraManager) param.thisObject;
+					CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+					if (disableCamera(characteristics, sharedPreferences))
+						param.setThrowable(new CameraAccessException(CameraAccessException.CAMERA_DISABLED));
+				}
+			});
+		}
+	}
+	
+	private static boolean disableCamera(CameraCharacteristics characteristics, SharedPreferences sharedPreferences) {
+		if (sharedPreferences.getBoolean("disableFrontFacing", true)
+				&& characteristics.get(CameraCharacteristics.LENS_FACING).equals(CameraCharacteristics.LENS_FACING_FRONT))
+			return true;
+		if (sharedPreferences.getBoolean("disableBackFacing", true)
+				&& characteristics.get(CameraCharacteristics.LENS_FACING).equals(CameraCharacteristics.LENS_FACING_BACK))
+			return true;
+		if (sharedPreferences.getBoolean("disableExternal", true)
+				&& characteristics.get(CameraCharacteristics.LENS_FACING).equals(CameraCharacteristics.LENS_FACING_EXTERNAL))
+			return true;
+		return false;
 	}
 }
--- app/src/main/java/com/programminghoch10/cameramanager/PackageHook.java
+++ app/src/main/java/com/programminghoch10/cameramanager/PackageHook.java
@@ -1,13 +1,20 @@
 package com.programminghoch10.cameramanager;
 
+import android.content.SharedPreferences;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class PackageHook implements IXposedHookLoadPackage {
 	@Override
 	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
 		if (lpparam.packageName.equals("android")) return;
-		//DisableHook.hook(lpparam);
-		CameraManagerHook.hook(lpparam);
+		SharedPreferences sharedPreferences = new XSharedPreferences(BuildConfig.APPLICATION_ID, "camera");
+		if (!sharedPreferences.getBoolean("available", false)) return;
+		if (sharedPreferences.getBoolean("disableCameraManager", false)) {
+			DisableHook.hook(lpparam);
+		} else {
+			CameraManagerHook.hook(lpparam, sharedPreferences);
+		}
 	}
 }
