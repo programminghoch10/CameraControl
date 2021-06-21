077364838c27357399173990d40f986a5b9639c5
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-06-21 19:52:23 +0200

    added xposed load package hook

---
+++ app/src/main/java/com/programminghoch10/cameramanager/PackageHook.java
@@ -0,0 +1,11 @@
+package com.programminghoch10.cameramanager;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class PackageHook implements IXposedHookLoadPackage {
+	@Override
+	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+		if (lpparam.packageName.equals("android")) return;
+	}
+}
